585f05339e52dbe826fc63d821a40f53764e0ea6
Harmeet Singh <harmeet@clausehound.com>
2018-07-26 16:15:55 -0400

    multiple timed out subscriptions

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import $ from 'jquery';
 import { Observable } from 'rxjs/Observable';
 import publish from 'rxjs/add/operator/publish';
+import interval from 'rxjs/add/observable/interval';
 import { getSubscriber } from './utils/getSubscriber';
 
-const source$ = Observable.create(observer => {
-  observer.next(Date.now());
-}).publish();
+const source$ = Observable.interval(2000).publish();
 
 source$.connect();
 
-source$.subscribe(getSubscriber('one'));
-source$.subscribe(getSubscriber('two'));
+setTimeout(() => {
+  source$.subscribe(getSubscriber('one'));
+  setTimeout(() => {
+    source$.subscribe(getSubscriber('two'));
+  }, 4000);
+}, 2000);
