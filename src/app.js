550eba58020c9bc818e2c3d5ac459deeab885809
Harmeet Singh <harmeet@clausehound.com>
2018-07-26 16:27:45 -0400

    observable range

--- src/app.js
+++ src/app.js
@@ -2,10 +2,9 @@ import $ from 'jquery';
 import { Observable } from 'rxjs/Observable';
 import publish from 'rxjs/add/operator/publish';
 import take from 'rxjs/add/operator/take';
-import timer from 'rxjs/add/observable/timer';
-import interval from 'rxjs/add/observable/interval';
+import range from 'rxjs/add/observable/range';
 import { getSubscriber } from './utils/getSubscriber';
 
-const source$ = Observable.timer(3000, 100)
+const source$ = Observable.range(0, 11)
   .take(10)
   .subscribe(getSubscriber('Interval'));
