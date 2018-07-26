696082be87c32ec55884d2a982b9d47ee1c05f55
Harmeet Singh <harmeet@clausehound.com>
2018-07-26 16:37:10 -0400

    observable promise

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 import $ from 'jquery';
 import { Observable } from 'rxjs/Observable';
 import publish from 'rxjs/add/operator/publish';
-import defer from 'rxjs/add/observable/defer';
+import fromPromise from 'rxjs/add/observable/fromPromise';
 import of from 'rxjs/add/observable/of';
 import { getSubscriber } from './utils/getSubscriber';
 
-let i = 0;
-
-const source$ = Observable.defer(function() {
-  i++;
-  return Observable.of(i);
+const myPromise = new Promise((resolve, reject) => {
+  console.log('Creating promise');
+  setTimeout(() => {
+    console.log('Something...');
+    resolve('Hello from promiseland!');
+  }, 2000);
 });
 
-source$.subscribe(getSubscriber('one'));
-source$.subscribe(getSubscriber('two'));
-source$.subscribe(getSubscriber('three'));
+Observable.fromPromise(myPromise).subscribe(getSubscriber('Promise'));
