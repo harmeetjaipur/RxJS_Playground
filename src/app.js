09875fdefbb669bb78e03542f5cd7228a832f634
Harmeet Singh <harmeet@clausehound.com>
2018-07-26 16:32:20 -0400

    individual sequential subscription

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import $ from 'jquery';
 import { Observable } from 'rxjs/Observable';
 import publish from 'rxjs/add/operator/publish';
-import take from 'rxjs/add/operator/take';
-import range from 'rxjs/add/observable/range';
+import defer from 'rxjs/add/observable/defer';
+import of from 'rxjs/add/observable/of';
 import { getSubscriber } from './utils/getSubscriber';
 
-const source$ = Observable.range(0, 11)
-  .take(10)
-  .subscribe(getSubscriber('Interval'));
+let i = 0;
+
+const source$ = Observable.defer(function() {
+  i++;
+  return Observable.of(i);
+});
+
+source$.subscribe(getSubscriber('one'));
+source$.subscribe(getSubscriber('two'));
+source$.subscribe(getSubscriber('three'));
