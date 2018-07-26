009882c21c8bfbda2a897d07261b73714d280819
Harmeet Singh <harmeet@clausehound.com>
2018-07-26 15:36:47 -0400

    traverse through an array using an observable

--- src/app.js
+++ src/app.js
@@ -1,35 +1,11 @@
 import $ from 'jquery';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/from';
 
-const input = document.getElementById('input');
-const button = document.getElementById('button');
-const coords = document.getElementById('coords');
+import { getSubscriber } from './utils/getSubscriber';
 
-const buttonStream$ = Observable.fromEvent(button, 'click');
+const nums = [34, 45, 56, 78, 89, 90];
 
-buttonStream$.subscribe(
-  x => {
-    console.log(x);
-  },
-  err => console.error(err),
-  () => console.log('Completed'),
-);
+const nums$ = Observable.from(nums);
 
-const inputStream$ = Observable.fromEvent(input, 'keyup');
-
-inputStream$.subscribe(
-  x => {
-    console.log(x.target.value);
-  },
-  err => console.err(err),
-  () => console.log('Completed'),
-);
-
-const mouseMove$ = Observable.fromEvent(document, 'mousemove').subscribe(
-  x => {
-    coords.innerHTML = x.clientX + ' ' + x.clientY;
-  },
-  err => console.error(err),
-  () => console.log('Completed'),
-);
+nums$.subscribe(getSubscriber('nums'));
