b63f876c5a6f5a7bf8fe19b51e9fc16f2bc69a88
Harmeet Singh <harmeet@clausehound.com>
2018-07-26 15:01:10 -0400

    simple event based observables

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
-alert(12);
+import $ from 'jquery';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+
+const input = document.getElementById('input');
+const button = document.getElementById('button');
+const coords = document.getElementById('coords');
+
+const buttonStream$ = Observable.fromEvent(button, 'click');
+
+buttonStream$.subscribe(
+  x => {
+    console.log(x);
+  },
+  err => console.error(err),
+  () => console.log('Completed'),
+);
+
+const inputStream$ = Observable.fromEvent(input, 'keyup');
+
+inputStream$.subscribe(
+  x => {
+    console.log(x.target.value);
+  },
+  err => console.err(err),
+  () => console.log('Completed'),
+);
+
+const mouseMove$ = Observable.fromEvent(document, 'mousemove').subscribe(x => {
+  coords.innerHTML = x.clientX + ' ' + x.clientY;
+});
