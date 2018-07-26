6316ae27ba89bc5a517285cf6a77be16e50a973a
Harmeet Singh <harmeet@clausehound.com>
2018-07-26 15:42:14 -0400

    observe a Set

--- src/app.js
+++ src/app.js
@@ -4,12 +4,18 @@ import 'rxjs/add/observable/from';
 
 import { getSubscriber } from './utils/getSubscriber';
 
-const users = [
-  { name: 'John Doe', email: 'jdoe@gmail.com' },
-  { name: 'Sam  Smith', email: 'ssmith@gmail.com' },
-  { name: 'Jen Thompson', email: 'jthompson@gmail.com' },
-];
+// const users = [
+//   { name: 'John Doe', email: 'jdoe@gmail.com' },
+//   { name: 'Sam  Smith', email: 'ssmith@gmail.com' },
+//   { name: 'Jen Thompson', email: 'jthompson@gmail.com' },
+// ];
 
-const users$ = Observable.from(users);
+// const users$ = Observable.from(users);
 
-users$.subscribe(getSubscriber('users'));
+// users$.subscribe(getSubscriber('users'));
+
+const s = new Set(['Foo', 43, { name: 'Jeff' }]);
+
+const s$ = Observable.from(s);
+
+s$.subscribe(getSubscriber('set'));
