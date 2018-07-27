3b047ceca059b7711d78a174138c3e139e2bf040
Harmeet Singh <harmeet@clausehound.com>
2018-07-27 10:32:31 -0400

    simple github user search app using observables

--- src/app.js
+++ src/app.js
@@ -2,15 +2,34 @@ import $ from 'jquery';
 import { Observable } from 'rxjs/Observable';
 import publish from 'rxjs/add/operator/publish';
 import fromPromise from 'rxjs/add/observable/fromPromise';
+import fromEvent from 'rxjs/add/observable/fromEvent';
 import of from 'rxjs/add/observable/of';
 import { getSubscriber } from './utils/getSubscriber';
 
-const myPromise = new Promise((resolve, reject) => {
-  console.log('Creating promise');
-  setTimeout(() => {
-    console.log('Something...');
-    resolve('Hello from promiseland!');
-  }, 2000);
+const input = document.getElementById('input');
+const profile = document.getElementById('profile');
+
+profile.style.display = 'none';
+
+Observable.fromEvent(input, 'keyup').subscribe(e => {
+  Observable.fromPromise(getGithubUser(e.target.value)).subscribe(user => {
+    profile.style.display = 'block';
+    document.getElementById('name').innerHTML = user.data.name || 'No name found.';
+    document.getElementById('login').innerHTML = user.data.login;
+    document.getElementById('blog').innerHTML = user.data.blog;
+    document.getElementById('avatar').innerHTML = user.data.avatar_url;
+    document.getElementById('repos').innerHTML = user.data.public_repos;
+    document.getElementById('followers').innerHTML = user.data.followers;
+    document.getElementById('following').innerHTML = user.data.following;
+    document.getElementById('link').setAttribute('href', user.data.html_url);
+  });
 });
 
-Observable.fromPromise(myPromise).subscribe(getSubscriber('Promise'));
+function getGithubUser(userName) {
+  return $
+    .ajax({
+      url: `https://api.github.com/users/${userName}?client_id=6a6b7b4c083f4d7a224d&client_secret=2f3879b89e0c6e104062530ac484e6fee8a7f5e6`,
+      dataType: 'jsonp',
+    })
+    .promise();
+}
