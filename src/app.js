f5bd492b5db78ec73f2727ac15798e50f82e7e95
Harmeet Singh <harmeet@clausehound.com>
2018-07-30 10:29:41 -0400

    find and select from observables

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ import $ from 'jquery';
 import { Observable } from 'rxjs/Observable';
 import map from 'rxjs/add/operator/map';
 import pluck from 'rxjs/add/operator/pluck';
+import first from 'rxjs/add/operator/first';
+import last from 'rxjs/add/operator/last';
+import find from 'rxjs/add/operator/find';
+import findIndex from 'rxjs/add/operator/findIndex';
+import single from 'rxjs/add/operator/single';
+import skip from 'rxjs/add/operator/skip';
+import skipUntil from 'rxjs/add/operator/skipUntil';
+import takeUntil from 'rxjs/add/operator/takeUntil';
+import skipWhile from 'rxjs/add/operator/skipWhile';
+import takeWhile from 'rxjs/add/operator/takeWhile';
 import take from 'rxjs/add/operator/take';
 import buffer from 'rxjs/add/operator/buffer';
 import bufferCount from 'rxjs/add/operator/bufferCount';
@@ -10,30 +20,47 @@ import interval from 'rxjs/add/observable/interval';
 import fromPromise from 'rxjs/add/observable/fromPromise';
 import fromEvent from 'rxjs/add/observable/fromEvent';
 import range from 'rxjs/add/observable/range';
+import timer from 'rxjs/add/observable/timer';
 import 'rxjs/add/observable/from';
 import of from 'rxjs/add/observable/of';
 import { getSubscriber } from './utils/getSubscriber';
 
-// TODO: implement bind operator to perform operations/funcs on Observables
+// TODO: implement `bind` operator to perform operations/funcs on Observables
 // obj::func
 // // is equivalent to:
 // func.bind(obj)
 
-// Observable.interval(1000)
-//   .buffer(Observable.interval(2000))
-//   .subscribe(getSubscriber('buffer'));
+// const source$ = new Observable(observer => {
+//   observer.next(1);
+//   observer.next(2);
+//   observer.next('Hello');
+//   observer.next('World');
 
-// Observable.range(1, 100)
-//   .bufferCount(4)
-//   .subscribe(getSubscriber('bcount'));
+//   observer.complete();
+// });
 
-// Observable.interval(1000)
-//   .bufferTime(5000)
-//   .subscribe(getSubscriber('btime'));
+// source$.first().subscribe(getSubscriber('First'));
 
-const obs1$ = Observable.interval(1000);
-const obs2$ = Observable.fromEvent(document, 'click');
+// source$.last().subscribe(getSubscriber('Last'));
 
-const myBuffer = obs1$.buffer(obs2$);
+// source$.single().subscribe(getSubscriber('Single'));
 
-const subscribe = myBuffer.subscribe(getSubscriber('Buffered  Values'));
+// source$.find((x, i, observer) => x === 'Hello').subscribe(getSubscriber('Find'));
+
+// source$.findIndex((x, i, observer) => x === 'Hello').subscribe(getSubscriber('Index Found'));
+
+// source$.take(2).subscribe(getSubscriber('Take'));
+
+// source$.skip(2).subscribe(getSubscriber('Skip'));
+
+// const source$ = Observable.range(1, 20);
+
+// source$
+//   .skipWhile(i => i <= 5)
+//   .takeWhile(i => i < 10)
+//   .subscribe(getSubscriber('Skip'));
+
+Observable.interval(500)
+  .skipUntil(Observable.timer(2000))
+  .takeUntil(Observable.timer(7000))
+  .subscribe(getSubscriber('Skip Until'));
